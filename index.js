c02b8bd8e41e5b2451aa6cd1cff4210c3f2acfb8
Tissyane <tissyane@gmail.com>
2022-08-27 21:33:18 -0300

    Post Sign-Up

--- index.js
+++ index.js
@@ -7,4 +7,12 @@ server.use(express.json());
 
 const port = "5000";
 
+const signup = [];
+const tweets = [];
+
+server.post("/sign-up", (req, res) => {
+  signup.push(req.body);
+  res.send("Ok");
+});
+
 server.listen(port);
