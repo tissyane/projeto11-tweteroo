f3e5094b6d097b2577ea1cbe393973650a4f4446
Tissyane <tissyane@gmail.com>
2022-08-27 21:58:31 -0300

    Get Tweets

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const port = "5000";
 const signup = [];
 const tweets = [];
 
+function getTweet(tweet) {
+  const avatar = signup.find(
+    (user) => user.username === tweet.username
+  )?.avatar;
+  return { ...tweet, avatar };
+}
+
 server.post("/sign-up", (req, res) => {
   signup.push(req.body);
   res.send("Ok");
@@ -21,3 +28,8 @@ server.post("/tweets", (req, res) => {
   res.send("Ok");
 });
 server.listen(port);
+
+server.get("/tweets", (req, res) => {
+  const lastTweets = tweets.slice(-10);
+  res.send(lastTweets.map(getTweet));
+});
